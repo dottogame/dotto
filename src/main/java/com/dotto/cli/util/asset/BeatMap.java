c60ebc0c202733939113e2f4b0411de95ad7cf44
SoraKatadzuma <hjoe822@gmail.com>
2018-03-06 12:53:53 -0600

    Added BeatMap objects A few objects associated with BeatMaps, including the BeatMap object itself.

---
+++ src/main/java/com/dotto/cli/util/asset/BeatMap.java
@@ -0,0 +1,33 @@
+package com.dotto.cli.util.asset;
+
+/**
+ * Contains all the necessary data for a {@code BeatMap}.
+ * 
+ * @author SoraKatadzuma
+ */
+public final class BeatMap {
+    /** The original version of this map. */
+    private final byte Version;
+    /** The updated version of this map. */
+    private final byte Revision;
+    /** The {@code Track} data for this {@code BeatMap}. */
+    private final Track TrackData;
+    /** The maps that represent this {@code BeatMap}. */
+    private final MapData[] Maps;
+    
+    /**
+     * Constructs a new instance of a {@code BeatMap} with the given
+     * {@code Track} data and the given maps.
+     * 
+     * @param Version The original version of this map.
+     * @param Revision The updated version of this map.
+     * @param TrackData The song information collected from the {@code index.json} file.
+     * @param Maps The Individual maps collected from the {@code index.json} file.
+     */
+    public BeatMap(byte Version, byte Revision, Track TrackData, MapData[] Maps) {
+        this.Version = Version;
+        this.Revision = Revision;
+        this.TrackData = TrackData;
+        this.Maps = Maps;
+    }
+}
---
+++ src/main/java/com/dotto/cli/util/asset/Track.java
@@ -0,0 +1,41 @@
+package com.dotto.cli.util.asset;
+
+/**
+ * This class contains information about a BeatMap track.
+ * 
+ * @author SoraKatadzuma
+ */
+public final class Track {
+    /** The name of this song. */
+    private final String TrackName;
+    /** The author of this song. */
+    private final String TrackAuthor;
+    /** The path to this track. */
+    private final String TrackPath;
+    /** The source this song came from, be it a game, show, or movie, etc. */
+    private final String Source;
+    /** The speed of this track. */
+    private final byte bpm;
+    /** The preview times for this track in milliseconds. */
+    private final int[] Preview;
+    
+    /**
+     * Constructs a new instance of the {@code Track} object with the
+     * given parameters.
+     * 
+     * @param TrackName The name of this track.
+     * @param TrackAuthor The name of the author of this track.
+     * @param bpm The speed of this track.
+     * @param Source The origination of this track.
+     * @param TrackPath The path to this track.
+     * @param Preview The preview times for this track in milliseconds.
+     */
+    public Track(String TrackName, String TrackAuthor, String TrackPath, String Source, byte bpm, int[] Preview) {
+        this.TrackName = TrackName;
+        this.TrackAuthor = TrackAuthor;
+        this.TrackPath = TrackPath;
+        this.Source = Source;
+        this.bpm = bpm;
+        this.Preview = Preview;
+    }
+}
