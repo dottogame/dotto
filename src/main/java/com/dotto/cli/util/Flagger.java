194a82651d280c2b030e23429589be1f59a8d737
SoraKatadzuma <hjoe822@gmail.com>
2018-03-06 09:23:40 -0600

    Added Flagger Tasked with setting application flags that other modules can check and reference without having access to the flag itself.

---
+++ src/main/java/com/dotto/cli/util/Flagger.java
@@ -0,0 +1,34 @@
+package com.dotto.cli.util;
+
+/**
+ * Responsible for setting flags that the command line argues.
+ * 
+ * @author SoraKatadzuma
+ */
+public final class Flagger {
+    /** Tells whether the application should run in debug mode. */
+    private static boolean DEBUG;
+    
+    /**
+     * Takes in the command line arguments and sets flags that correspond to their names.
+     * 
+     * @param cla The command line arguments.
+     */
+    public static void setFlags(String... cla) {
+        for (String arg : cla)
+            switch (arg) {
+                case "debug":
+                    DEBUG = true;
+                    break;
+            }
+    }
+    
+    /**
+     * Returns whether or not the application is in debug mode.
+     * 
+     * @return Whether or not the application is in debug mode.
+     */
+    public static boolean DebugMode() {
+        return DEBUG;
+    }
+}
