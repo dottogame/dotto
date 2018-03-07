97c5c9e70ef9aea4f77abcfb0586be9735c3c778
SoraKatadzuma <hjoe822@gmail.com>
2018-03-07 14:04:17 -0600

    Score changes Made some changes to make the Score manager more intuitive.

--- src/main/java/com/dotto/cli/util/manager/Score.java
+++ src/main/java/com/dotto/cli/util/manager/Score.java
@@ -6,27 +6,51 @@ package com.dotto.cli.util.manager;
  * 
  * @author SoraKatadzuma
  */
-public final class Score {    
+public final class Score {
+    /** The accuracy score the player achieved. */
+    public static int AccuracyScore;
+    /** The player's current combo. */
+    public static int Combo;
+    /** The player's current score. */
+    public static int CurrentScore;
+    /** The player's current accuracy. */
+    private static float CurrentAccuracy = 100f;
+    
     /**
      * Calculates the score based on the timing of the current
      * {@code Beat}, combo, and current score.
      * 
      * @param ms The time in milliseconds it took for the player to click the {@code Beat}.
      *      It should always be positive.
-     * @param combo The current combo the player is holding.
-     * @param currentScore The score the player is currently holding.
      * @return The score based on the timing of the current
      *      {@code Beat}, combo, and current score.
      */
-    public static int CalculateScore(int ms, int combo, int currentScore) {
-        int score;
-        
-        score = (ms <= 75) ? 500 :
+    public static int CalculateScore(int ms) {
+        AccuracyScore = (ms <= 75) ? 500 :
                 (ms <= 115) ? 300 :
                 (ms <= 150) ? 150 :
                 (ms <= 300) ? 75 :
                 (ms <= 500) ? 30 : 0;
         
-        return score * combo + currentScore;
+        return AccuracyScore * Combo + CurrentScore;
+    }
+    
+    /**
+     * Calculates the player's accuracy after clicking a {@code Beat}.
+     * 
+     * @param noOfBeats The number of beats that have been played.
+     * @return The calculated accuracy the player currently has.
+     */
+    public static float CalculateAccuracy(int noOfBeats) {
+        int accReduction;
+        
+        accReduction = (AccuracyScore == 500) ? 0 :
+                       (AccuracyScore == 300) ? 15 :
+                       (AccuracyScore == 150) ? 30 :
+                       (AccuracyScore == 75) ? 50 :
+                       (AccuracyScore == 30) ? 75 :
+                       (AccuracyScore == 0) ? 100 : 100;
+        
+        return CurrentAccuracy = CurrentAccuracy - (float)(accReduction / noOfBeats);
     }
 }
