64326a0b574a71070d8da54948bc5cd31b4eec37
lite20 <lite260@gmail.com>
2018-03-10 14:52:05 -0600

    take a dump on the quality

--- src/main/java/com/dotto/cli/GamePane.java
+++ src/main/java/com/dotto/cli/GamePane.java
@@ -35,7 +35,7 @@ public class GamePane extends JPanel implements MouseListener, KeyListener {
      */
     public GamePane() {
         setOpaque(true);
-        
+
         // The starting view of the game.
         view = new Boot();
 
@@ -64,23 +64,45 @@ public class GamePane extends JPanel implements MouseListener, KeyListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        Graphics2D g2 = (Graphics2D) g;
+        Graphics2D g2 = (Graphics2D) g.create();
         if (Config.ANTIALIAS) {
-            RenderingHints rh = new RenderingHints(
+            g2.setRenderingHint(
                 RenderingHints.KEY_TEXT_ANTIALIASING,
                 RenderingHints.VALUE_TEXT_ANTIALIAS_ON
             );
 
-            g2.setRenderingHints(rh);
-        }
+            g2.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON
+            );
+        } else {
+            g2.setRenderingHint(
+                RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_OFF
+            );
 
-        g2.setRenderingHint(
-            RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE
-        );
+            g2.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_OFF
+            );
+        }
 
         g2.setRenderingHint(
             RenderingHints.KEY_INTERPOLATION,
             RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR
+        );
+
+        g2.setRenderingHint(
+            RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_SPEED
+        );
+
+        g2.setRenderingHint(
+            RenderingHints.KEY_ALPHA_INTERPOLATION,
+            RenderingHints.VALUE_ALPHA_INTERPOLATION_SPEED
+        );
+
+        g2.setRenderingHint(
+            RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON
         );
 
         view.draw(g2);
