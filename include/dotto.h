b42d9048cfa97418358695b0953458b477f4b034
SoraKatadzuma <dungeonsandanime@gmail.com>
2018-12-04 16:38:28 -0600

    Precompiled header
    Change from regular header to precompiled header

--- include/dotto.h
+++ include/dotto.h
@@ -1,12 +1,5 @@
-/* Copyright (c) 2018 LiteMcFish, SoraKatadzuma
- * Licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives
- * 4.0 International Public License
- */
-
 #ifndef _DOTTO_MAIN_HEADER
 #define _DOTTO_MAIN_HEADER
-#include <iostream>
-#include "gl/glew.h"
-#include "glfw/glfw3.h"
+#include "pch.h"
 
 #endif
---
+++ include/pch.h
@@ -0,0 +1,7 @@
+#ifndef _DOTTO_PRECOMPILED_HEADER
+#define _DOTTO_PRECOMPILED_HEADER
+#include <iostream>
+#include "gl/glew.h"
+#include "glfw/glfw3.h"
+
+#endif
