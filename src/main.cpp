6ecdc4785d4fa49abe356eada8e8fc7a21278052
SoraKatadzuma <dungeonsandanime@gmail.com>
2018-12-04 16:52:45 -0600

    Window creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,56 @@
 #include <dotto.h>
 
 int main(int argc, char** argv) {
+    /* Initialize GLFW. */
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW.\n";
+        return EXIT_FAILURE;
+    }
 
+    // Request OpenGL core version 4.4
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLWF_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+
+    // Create window.
+    GLFWmonitor* primary    = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(primary);
+    GLFWwindow* window      = glfwCreateWindow(
+        mode->width,
+        mode->height,
+        "Dotto",
+        primary,
+        NULL
+    );
+
+    // Check if Window exists.
+    if (!window) {
+        std::cerr << "Failed to create window.\n";
+        return EXIT_FAILURE;
+    }
+
+    // Initialize GLEW.
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW.\n";
+        return EXIT_FAILURE;
+    }
+
+    // Perform window actions.
+    while (!glfwShouldCloseWindow(window)) {
+        glfwPollEvents();
+
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(true);
+
+        glfwSwapBuffers(window);
+    }
+
+    // Destroy window if it still exists.
+    if (window)
+        glfwDestroyWindow(window);
+
+    // Terminate GLFW.
+    glfwTerminate();
 
     return EXIT_SUCCESS;
 }
