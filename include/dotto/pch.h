d5f0999fc4cdaace0e1796779a35176db53bdad8
SoraKatadzuma <dungeonsandanime@gmail.com>
2018-12-06 00:10:38 -0600

    Loop timing

--- include/dotto/pch.h
+++ include/dotto/pch.h
@@ -1,33 +1,37 @@
 #ifndef _DOTTO_PRECOMPILED_HEADER
 #define _DOTTO_PRECOMPILED_HEADER
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <thread>
 #include <unordered_map>
 #include <vector>
 #include "gl/glew.h"
 #include "glfw/glfw3.h"
 #include "glm/glm.hpp"
 #include "glm/ext.hpp"
-// #include "dotto.pb.h"
 
 #ifdef _WIN32
 #define UNICODE
-
-#ifdef __MINGW32__
-#define NOMINMAX
-#endif
-
 #define STRICT
 #define VC_EXTRALEAN
 #define WIN32_LEAN_AND_MEAN
+
+#ifndef __MINGW32__
+#define NOMINMAX
+#endif // __MINGW32__
+
 #include <Windows.h>
+
 #undef UNICODE
-#undef NOMINMAX
 #undef STRICT
 #undef VC_EXTRALEAN
 #undef WIN32_LEAN_AND_MEAN
-#endif
 
-#endif
+#ifndef __MINGW32__
+#undef NOMINMAX
+#endif // __MINGW32__
+#endif // _WIN32
+#endif // _DOTTO_PRECOMPILED_HEADER
--- src/main.cpp
+++ src/main.cpp
@@ -59,8 +59,17 @@ int main(int argc, char** argv) {
     glUniformMatrix4fv(u_view, 1, false, &view[0][0]);
     glUniformMatrix4fv(u_model, 1, false, &model[0][0]);
 
+    // For timings.
+    using namespace std::chrono_literals;
+    using clock = std::chrono::high_resolution_clock;
+    constexpr std::chrono::nanoseconds timestep(16ms);
+    float delta_time;
+
     // Perform window actions.
     while (wnd.is_open()) {
+        // Start clock.
+        auto start = clock::now();
+
         // Check if we should escape the window.
         if (glfwGetKey((GLFWwindow*)wnd, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose((GLFWwindow*)wnd, true);
@@ -79,6 +88,13 @@ int main(int argc, char** argv) {
         // Swap back and front buffer.
         wnd.swap_buffers();
         wnd.poll_events();
+
+        // Calculate Delta time.
+        auto end = clock::now();
+        auto length_ns = end - start;
+        auto wait_ns = timestep - length_ns;
+        std::this_thread::sleep_for(wait_ns);
+        delta_time = (wait_ns + length_ns).count() / 1000000000.0;
     }
 
     return EXIT_SUCCESS;
